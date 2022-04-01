65b390f3753e8d759fadd434cbc6736434616fb9
RamtinPRG <ramtin.barzkar.3@gmail.com>
2022-04-02 04:11:19 +0430

    Add account type models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Nurse(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
