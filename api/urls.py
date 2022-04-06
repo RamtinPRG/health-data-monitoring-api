02acb573fc5dda454425c362fde017253dcee72a
RamtinPRG <ramtin.barzkar.3@gmail.com>
2022-04-06 21:42:46 +0430

    Support JWT authentication and authorization  method

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('authentication-status', views.authentication_status, name='authentication-status'),
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.contrib.auth import login, logout, authenticate
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from . import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from . import serializers, models
 
 
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['email'] = user.email
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 @api_view(['POST'])
 def signup(request):
@@ -30,12 +52,34 @@ def login_view(request):
     if request.user.is_authenticated:
         return Response({'error': 'You are already logged in.'})
     else:
+        # serializer = serializers.UserSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     username = request.data.get('username')
+        #     password = request.data.get('password')
+        #     user = authenticate(username=username, password=password)
+        #     if user is not None:
+        #         login(request, user)
+        #         return Response({'success': 'Logged in successfully.'})
+        #     else:
+        #         return Response({'error': 'Invalid username or password.'})
+        # else:
+        #     errors = serializer.errors
+        #     errors['error'] = 'Invalid data'
+        #     return Response(errors)
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username, password=password)
         if user is not None:
-            login(request, user)
-            return Response({'success': 'Logged in successfully.'})
+            serializer = serializers.UserSerializer(user, request.data)
+            if serializer.is_valid():
+                login(request, user)
+                data = serializer.data
+                data['success'] = 'Logged in successfully.'
+                return Response(data)
+            else:
+                errors = serializer.errors
+                errors['error'] = 'Invalid data'
+                return Response(errors)
         else:
             return Response({'error': 'Invalid username or password.'})
 
