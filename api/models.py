d3ae3beb29e468d36b392d8a0f7cde7439b11f93
RamtinPRG <ramtin.barzkar.3@gmail.com>
2022-04-06 21:36:38 +0430

    Update: Change user model to an AbstractUser instance

--- api/models.py
+++ api/models.py
@@ -1,16 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
+class User(AbstractUser):
+    ACCOUNT_TYPES = (
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor'),
+        ('nurse', 'Nurse'),
+    )
+    account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPES, default='patient')
+    patient = models.OneToOneField('Patient', on_delete=models.CASCADE)
+    doctor = models.OneToOneField('Doctor', on_delete=models.CASCADE)
+    inspector = models.OneToOneField('Inspector', on_delete=models.CASCADE)
+
+
 class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ecg = models.JSONField(default={'data': []})
 
 
 class Doctor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    patients = models.ManyToManyField(Patient)
 
 
-class Nurse(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Inspector(models.Model):
+    patients = models.ManyToManyField(Patient)
--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,14 @@
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
+from . import models
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
-        model = User
+        model = models.User
         fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = models.User.objects.create_user(**validated_data)
+        return user
