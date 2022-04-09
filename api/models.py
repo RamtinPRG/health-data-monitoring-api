4e2d273ba96112a5094a820ef40232b6d3260051
RamtinPRG <ramtin.barzkar.3@gmail.com>
2022-04-09 10:31:45 +0430

    Implement track request model and serializer

--- api/models.py
+++ api/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+def default_ecg_dictionary():
+    return {"data": []}
+
 # Create your models here.
 
 
@@ -8,16 +12,19 @@ class User(AbstractUser):
     ACCOUNT_TYPES = (
         ('patient', 'Patient'),
         ('doctor', 'Doctor'),
-        ('nurse', 'Nurse'),
+        ('inspector', 'Inspector'),
     )
     account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPES, default='patient')
-    patient = models.OneToOneField('Patient', on_delete=models.CASCADE)
-    doctor = models.OneToOneField('Doctor', on_delete=models.CASCADE)
-    inspector = models.OneToOneField('Inspector', on_delete=models.CASCADE)
+    patient = models.OneToOneField('Patient', on_delete=models.CASCADE, null=True, blank=True)
+    doctor = models.OneToOneField('Doctor', on_delete=models.CASCADE, null=True, blank=True)
+    inspector = models.OneToOneField('Inspector', on_delete=models.CASCADE, null=True, blank=True)
 
 
 class Patient(models.Model):
-    ecg = models.JSONField(default={'data': []})
+    ecg = models.JSONField(default=default_ecg_dictionary)
+    body_temp = models.FloatField(default=0)
+    spo2 = models.FloatField(default=0)
+    heart_rate = models.IntegerField(default=0)
 
 
 class Doctor(models.Model):
@@ -26,3 +33,18 @@ class Doctor(models.Model):
 
 class Inspector(models.Model):
     patients = models.ManyToManyField(Patient)
+
+
+class Request(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    )
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    sender_visisbility = models.BooleanField(default=True)
+    receiver_visibility = models.BooleanField(default=True)
--- api/serializers.py
+++ api/serializers.py
@@ -10,5 +10,42 @@ class UserSerializer(ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = models.User.objects.create_user(**validated_data)
+        patient = models.Patient()
+        patient.save()
+        doctor = models.Doctor()
+        doctor.save()
+        inspector = models.Inspector()
+        inspector.save()
+        user = models.User.objects.create_user(**validated_data, patient=patient, doctor=doctor, inspector=inspector)
         return user
+
+
+class PatientSerializer(ModelSerializer):
+    class Meta:
+        model = models.Patient
+        fields = ('id', 'ecg', 'body_temp', 'spo2', 'heart_rate')
+
+
+class DoctorSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = models.Doctor
+        fields = ('id', 'user')
+
+
+class InspectorSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = models.Inspector
+        fields = ('id', 'user')
+
+
+class RequestSerializer(ModelSerializer):
+    sender = UserSerializer(read_only=True)
+    receiver = UserSerializer(read_only=True)
+
+    class Meta:
+        model = models.Request
+        fields = ('id', 'sender', 'receiver', 'message', 'timestamp', 'status')
