c346a38b25c5bc7202f36f9debbfd5b30316d169
RamtinPRG <ramtin.barzkar.3@gmail.com>
2022-04-14 22:05:46 +0430

    Change JWT access token life time

--- health_data_monitoring/settings.py
+++ health_data_monitoring/settings.py
@@ -150,7 +150,7 @@ REST_FRAMEWORK = {
 # Django REST framework simplejwt settings
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=90),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
